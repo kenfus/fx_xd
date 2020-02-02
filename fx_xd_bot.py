4a5af1ed11c8538f18057aad84d2ae0c25c1c211
Kenfus <vincenzo.timmel@gmail.com>
2020-02-02 18:32:59 +0100

    *Improved data-feed

--- fx_xd_bot.py
+++ fx_xd_bot.py
@@ -1,20 +1,22 @@
 import fxcmpy
 import datetime as dt
 import backtrader as bt
+import plotly
 
 #
 ###
 #Define Parameters here!
 token_to_trade = 'EUR/GBP'
 time_frame = 'D1'
-start_dt = dt.datetime(2019, 1, 1)
+start_dt = dt.datetime(2017, 1, 1)
 stop_dt = dt.datetime(2020, 1, 31)
 server_type = 'demo' # server = 'real' for live
 config_file_path = 'fxcm.cfg'
+# Define the renaming and which columns to use for this test. Careful, the columns which are not defined or renamed here will be dropped!
 renaming = {'bidopen': 'open', 'bidclose': 'close', 'bidhigh':'high', 'bidlow':'low', 'tickqty':'volume'}
 timeframe = bt.TimeFrame.Days
 cash_amount = 10000
-leverage = 100
+leverage = 50
 order_size = 0.2 * cash_amount
 commission = 0.001
 ###
@@ -52,34 +54,31 @@ class SmaCross(bt.Strategy):
 
 ###
 class Strat1(bt.Strategy):
-  # list of parameters which are configurable for the strategy
-  params = dict(
-      pfast=20,  # period for the fast moving average
-      pslow=30   # period for the slow moving average
-  )
-
   def __init__(self):
-      sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
-      sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
-      self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
       self.atr = bt.ind.AverageTrueRange(period = 14)
-      self.williamsr = bt.ind.WilliamsR(period = 14, upperband = -20, lowerband = -80)
-      self.momentum = bt.ind.Momentum(period = 12)
-      self.laguerre = bt.ind.LaguerreFilter(period = 1)
+      self.williamsr = bt.ind.WilliamsR(period = 7)
+      self.laguerre = bt.ind.LaguerreFilter(period = 7)
 
   def next(self):
       if not self.position:  # not in the market
-            if self.williamsr < -30:  # if fast crosses slow to the upside
-                if self.momentum > 0:
+            if self.williamsr > -20: 
+                if self.laguerre[0] < self.data[0]:
+                    print(self.data[0], self.williamsr[0], self.laguerre[0])
                     self.buy(size = order_size) # enter long
 
-      elif self.momentum < -0.03:  # in the market & cross to the downside
-          self.close()  # close long position      
-### Helper Functions:
+      elif self.williamsr < -80:  # in the market & cross to the downside
+          self.close()  # close long position  
+          
+### Helper Functions
+columns_to_keep = []
+for key, value in renaming.items():
+    columns_to_keep.append(key)
+    
 def transform_data(df):
     return fxcm_df_to_bt_df(df, start_dt, stop_dt, token_to_trade, time_frame, renaming)
 
 def fxcm_df_to_bt_df(df, start_dt, stop_dt, token_to_trade, time_frame, renaming):
+    df = df[columns_to_keep].copy()
     df.rename(columns = renaming, inplace = True)
     return df
 ###
@@ -92,9 +91,10 @@ cerebro.addstrategy(Strat1)
 
 # Transform data
 dataframe = transform_data(data)
+print(dataframe.head())
 
 # Transform and feed data to backtrader and set parameters for the broker
-data_to_backtest = bt.feeds.PandasData(dataname=dataframe, timeframe=timeframe, openinterest=None)
+data_to_backtest = bt.feeds.PandasData(dataname=dataframe, timeframe=timeframe, openinterest = None)
 cerebro.adddata(data_to_backtest)
 
 # Set our desired cash start
@@ -115,4 +115,5 @@ end_value_leverage = starting_value + (end_value - starting_value) * leverage
 print('Final Portfolio Value: %.2f' % end_value_leverage)
 
 # Plot the results
-cerebro.plot(openinterest = None, volume = None, iplot=False)
+result = cerebro.plot()
+result
