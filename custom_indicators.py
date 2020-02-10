2245603054de1bb6e6e6a4bf6329db196bffd9cd
Kenfus <vincenzo.timmel@gmail.com>
2020-02-10 12:38:51 +0100

    Added custom_indicator file

--- custom_indicators.py
+++ custom_indicators.py
@@ -1,32 +1,7 @@
 import backtrader as bt
 import numpy as np
 
-'''
-length = input(10)
-fastLength = input(23)
-slowLength = input(50)
-macd(source, fastLength, slowLength) = >
-fastMA = ema(source, fastLength)
-slowMA = ema(source, slowLength)
-macd = fastMA - slowMA
-macd
-
-stc(length, fastLength, slowLength) = >
-factor = input(0.5)
-m = macd(close, fastLength, slowLength)
-v1 = lowest(m, length)
-v2 = highest(m, length) - v1
-f1 = (v2 > 0 ? ((m - v1) / v2) *100: nz(f1[1]))
-pf = (na(pf[1]) ? f1: pf[1] + (factor * (f1 - pf[1])))
-v3 = lowest(pf, length)
-v4 = highest(pf, length) - v3
-f2 = (v4 > 0 ? ((pf - v3) / v4) *100: nz(f2[1]))
-pff = (na(pff[1]) ? f2: pff[1] + (factor * (f2 - pff[1])))
-pff
-'''
-
-
-class schaff_trend_cycle(bt.Indicator):
+class schaff_trend_cycle(bt.ind.PeriodN):
     lines = ('schaff_cycle', 'f1', 'f2', 'pf',)
 
     params = (('length', 20),
